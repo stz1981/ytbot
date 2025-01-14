57a97f151162170abc1e6059b9efd19eee5ab20c
STZ <67462716+stz1981@users.noreply.github.com>
2025-01-14 11:34:56 +0400

    setup_and_run.sh

---
+++ setup_and_run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Update and install dependencies
+apt-get update
+apt-get install -y ffmpeg cron
+
+# Install Python dependencies
+pip install --no-cache-dir -r requirements.txt
+
+# Add crontab file in the cron directory
+cp crontab /etc/cron.d/container-cron
+
+# Give execution rights on the cron job
+chmod 0644 /etc/cron.d/container-cron
+
+# Apply cron job
+crontab /etc/cron.d/container-cron
+
+# Create the log file to be able to run tail
+touch /var/log/cron.log
+
+# Start cron and the bot script
+cron
+python ./bot.py
